94cf2187d67495bfeb3fa494d3aaa77da222d2dc
ItIsEternity <lavrovslava207@gmail.com>
2023-09-29 22:22:29 +0300

    Updates

--- _nuxt/default.82a4175c.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,q as o,M as a,s as e,m as c}from"./entry.0f961f65.js";const _={},n={class:"layout"},r=e("div",{class:"header"},[e("h2",{class:"title"},"Kroshechkin"),e("h3",{class:"description"},"My projects")],-1);function l(s,d,i,p,u,f){return c(),o("div",n,[r,a(s.$slots,"default")])}const m=t(_,[["render",l]]);export{m as default};
